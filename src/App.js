8c295a7e0d91aa64c746b5ed08a23c62af2af6d8
alexandermaggs <75097869+alexandermaggs@users.noreply.github.com>
2022-02-05 10:28:43 +0000

    Function create digits

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 function App() {
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button key={i}>{i}</button>);
+    }
+    return digits;
+  };
+
   return (
     <div className="App">
       <div className="calculator">
@@ -11,6 +20,12 @@ function App() {
           <button>+</button>
           <button>-</button>
           <button>DEL</button>
+        </div>
+        <div className="digits">
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
         </div>
       </div>
     </div>
